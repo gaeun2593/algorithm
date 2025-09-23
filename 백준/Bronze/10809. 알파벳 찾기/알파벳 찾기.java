5277ec18170aa6d8b167720684ae461a138b0c57
gaeunii <gaeun2593@gmail.com>
2025-09-23 16:09:11 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 192 ms, Memory: 18436 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int[] a = new int[26];
+
+		for (int i = 0; i < 26; i++) {
+			a[i] = -1;
+		}
+
+		String s = sc.nextLine();
+
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+
+			if (a[ch - 'a'] == -1) {
+				a[ch - 'a'] = i;
+			}
+		}
+
+		for (int i = 0; i < 26; i++) {
+			System.out.print(a[i] + " ");
+		}
+	}
+}
